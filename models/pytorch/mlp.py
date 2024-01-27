a44ca77795475076c1fe8b3cbdcf9930e59e7837
clement.labrugere <clement.labrugere@teads.com>
2024-01-28 00:01:41 +0100

    Refactor MLP

--- models/pytorch/mlp.py
+++ models/pytorch/mlp.py
@@ -1,25 +1,22 @@
 from torch import nn
 
 
-class MLP(nn.Module):
+class MLP(nn.Sequential):
     def __init__(self, dim_in, num_hidden, dim_hidden, dim_out=None, batch_norm=True, dropout=0.0):
-        super().__init__()
-
-        self.layers = nn.Sequential()
+        layers = []
         for _ in range(num_hidden - 1):
-            self.layers.append(nn.Linear(dim_in, dim_hidden))
+            layers.append(nn.Linear(dim_in, dim_hidden))
 
             if batch_norm:
-                self.layers.append(nn.BatchNorm1d(dim_hidden))
+                layers.append(nn.BatchNorm1d(dim_hidden))
 
-            self.layers.append(nn.ReLU())
-            self.layers.append(nn.Dropout(dropout))
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout(dropout))
             dim_in = dim_hidden
 
         if dim_out:
-            self.layers.append(nn.Linear(dim_in, dim_out))
+            layers.append(nn.Linear(dim_in, dim_out))
         else:
-            self.layers.append(nn.Linear(dim_in, dim_hidden))
+            layers.append(nn.Linear(dim_in, dim_hidden))
 
-    def forward(self, inputs):
-        return self.layers(inputs)
+        super().__init__(*layers)
